a82cfdb52712242e2b6515a3975254d514fcc2ab
larexx40 <larexx40@gmail.com>
2022-03-28 09:51:37 +0100

    Find location

--- controller/openWeather.js
+++ controller/openWeather.js
@@ -45,3 +45,35 @@ exports.getHome =  (req, res)=>{
         }
     })
 }
+
+exports.findLocation = (req, res)=>{
+    var city = req.body.city
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apikey}`
+
+    request(url, (err, response, result)=>{
+        if(err){
+            console.log(err);
+        }else{
+            let  weather = JSON.parse(result);
+            console.log();  
+
+            const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+            const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+            const date = new Date();
+            const today = date.getDate();
+            const day = days[date.getDay()];
+            const month = months[date.getMonth()];
+            const year = date.getFullYear();
+
+            res.render('index', {
+                title: 'open weather api', 
+                weather: weather, 
+                error: null, 
+                date: date.toDateString,
+                day: day,
+                month: month
+            })
+             
+        }
+    })
+}
