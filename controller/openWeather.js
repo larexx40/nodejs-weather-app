22d0b497750e7ced306ee887dad4a82a8e96db6f
larexx40 <larexx40@gmail.com>
2022-04-02 15:34:20 +0100

    view done

--- controller/openWeather.js
+++ controller/openWeather.js
@@ -2,9 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const config = require('../config');
 const request = require('request');
-const { response } = require('../app');
-
-var openWeather = express.Router();
 const apiKey = config.APIKey;
 
 
@@ -21,58 +18,84 @@ exports.getHome =  (req, res)=>{
             weather = JSON.parse(result);
             console.log(weather);
 
+            const f =  weather.main.temp * 1.8 + 32;
+            const fahrenheit = f.toFixed(2);
+            const weatherIcon = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
             const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
             const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-            var date = new Date();
-            var day = date.getDay().toString();
-            var today = date.getDate();
-            var month = date.getMonth();
-            var year = date.getFullYear();
-            console.log(days[day] + " " + date + " " + months[month] + "; " + year );
-            console.log(date.toDateString());
-            console.log(today.toString());
-            console.log(month.toString());
-            console.log(year.toString());
+            var today = new Date();
+            var day = today.getDay()
+            var date = today.getDate();
+            var month = today.getMonth();
+            var year = today.getFullYear();
+            console.log("The time and date function");
+            console.log(today);
+            console.log(days[day]);
+            console.log(date);
+            console.log(months[month]);
+            console.log(year);
             
-            res.render('index', { 
-                title: 'open weather api', 
-                weather: result , 
+            res.render('index.ejs', { 
+                title: 'open weather api',
+                weather: weather , 
                 error: null, 
-                date: date.toDateString,
+                today: today,
                 day: days[day],
-                month: months[month]
+                date: date,
+                month: months[month],
+                fahrenheit: fahrenheit,
+                icon: weatherIcon,
             });
         }
     })
 }
 
 exports.findLocation = (req, res)=>{
-    var city = req.body.city
+    const {city} = req.body;
+    const apikey = config.APIKey;
+    console.log(city);
+    //fetch data using city name
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apikey}`
 
     request(url, (err, response, result)=>{
         if(err){
-            console.log(err);
+            res.render('index', { title: 'open weather api', weather: null, error: 'Error pls try again' })
         }else{
-            let  weather = JSON.parse(result);
-            console.log();  
+            weather = JSON.parse(result);
+            console.log(weather);
 
+
+            //console.log(weather.name);  
+            const f =  weather.main.temp * 1.8 + 32;
+            const fahrenheit = f.toFixed(2);
+            const weatherIcon = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
+            console.log(weatherIcon);
+                        
             const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
             const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-            const date = new Date();
-            const today = date.getDate();
-            const day = days[date.getDay()];
-            const month = months[date.getMonth()];
-            const year = date.getFullYear();
-
-            res.render('index', {
-                title: 'open weather api', 
-                weather: weather, 
+            var today = new Date();
+            var day = today.getDay()
+            var date = today.getDate();
+            var month = today.getMonth();
+            var year = today.getFullYear();
+            console.log("The time and date function");
+            console.log(today);
+            console.log(days[day]);
+            console.log(date);
+            console.log(months[month]);
+            console.log(year);
+            
+            res.render('index.ejs', { 
+                title: 'open weather api',
+                weather: weather , 
                 error: null, 
-                date: date.toDateString,
-                day: day,
-                month: month
-            })
+                today: today,
+                day: days[day],
+                date: date,
+                month: months[month],
+                fahrenheit: fahrenheit,
+                icon: weatherIcon,
+            });
              
         }
     })
